cec287965419781d8f52650bddb84df47f2b4f73
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-16 13:06:11 -0500

    Another commit:

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,18 +6,18 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please guess a number between 1 and 10.");
-            string userGuess = Console.ReadLine();
-            Console.WriteLine("You guessed: " + userGuess);
+            Console.WriteLine("Please guess a number between 1 and 10.");  //Asks the user to input a number
+            string userGuess = Console.ReadLine();            //Waits for the user to input a number
+            Console.WriteLine("You guessed: " + userGuess);   //Concatenates You guessed with user input
 
-            if (userGuess.Equals("7"))
+            if (userGuess.Equals("7"))   //If input is 7 they win
             {
                 Console.WriteLine("You win!");
             }
 
             else
             {
-                Console.WriteLine("You lost");
+                Console.WriteLine("You lost");  //If input is anything other than 7 they lose
             }
 
         }
