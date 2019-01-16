746df9a6a2deded8ac8c0dd12fe6af634edefc88
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-16 12:09:17 -0500

    Another Commit

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,8 +6,20 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Adios!");
+            Console.WriteLine("Please guess a number between 1 and 10.");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You guessed: " + userGuess);
+
+            if (userGuess.Equals("7"))
+            {
+                Console.WriteLine("You win!");
+            }
+
+            else
+            {
+                Console.WriteLine("You lost");
+            }
+
         }
     }
 }
